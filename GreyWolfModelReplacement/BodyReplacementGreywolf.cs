697f9920fc936bae1c22d75f5abe6ae0752b38f2
alekso56 <alekso56@users.noreply.github.com>
2023-12-03 22:11:42 +0100

    Add more

---
+++ GreyWolfModelReplacement/BodyReplacementGreywolf.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Xml;
+using System.IO;
+using GameNetcodeStuff;
+using UnityEngine.PlayerLoop;
+using System.Reflection;
+using ModelReplacement;
+
+namespace GreyWolfModelReplacement
+{
+    public class BodyReplacementGreywolf : BodyReplacementBase
+    {
+        public override string boneMapFileName => "bonezonegreywolf.json";
+
+        public override GameObject LoadAssetsAndReturnModel()
+        {
+            return LC_API.BundleAPI.BundleLoader.GetLoadedAsset<GameObject>("assets/greywolf/greywolf.prefab");
+        }
+
+        public override void AddModelScripts()
+        {
+
+            var HairBones = replacementModel.GetComponentsInChildren<Transform>().Where(x => x.name.Contains("FrontHair") || x.name.Contains("SideHair"));
+            HairBones.ToList().ForEach(bone =>
+            {
+                DynamicBone dynBone = bone.gameObject.AddComponent<DynamicBone>();
+                dynBone.m_Root = bone;
+                dynBone.m_UpdateRate = 60;
+                dynBone.m_Damping = 0.254f;
+                dynBone.m_Elasticity = 0.08f;
+                dynBone.m_Stiffness = 0.5f;
+                dynBone.m_Inert = 0.274f;
+                dynBone.m_Radius = 0.05f;
+                dynBone.m_Gravity = new Vector3(0, -0.01f, 0);
+            });
+        }
+
+ 
+    }
+}
---
+++ GreyWolfModelReplacement/Plugin.cs
@@ -0,0 +1,64 @@
+﻿using BepInEx;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Zeekerss;
+using Zeekerss.Core;
+using Zeekerss.Core.Singletons;
+using System;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using System.Reflection;
+using GameNetcodeStuff;
+using ModelReplacement;
+
+//using System.Numerics;
+
+namespace GreyWolfModelReplacement
+{
+
+
+
+    [BepInPlugin("GreywolfModelReplacement", "Greywolf Model", "1.0")]
+    [BepInDependency("meow.ModelReplacementAPI", BepInDependency.DependencyFlags.HardDependency)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        private void Awake()
+        {
+            // Plugin startup logic
+
+            ModelReplacementAPI.RegisterSuitModelReplacement("Green Suit", typeof(BodyReplacementGreywolf));
+            ModelReplacementAPI.RegisterSuitModelReplacement("Default", typeof(BodyReplacementEruqyuna));
+            ModelReplacementAPI.RegisterSuitModelReplacement("Orange suit", typeof(BodyReplacementEruqyuna));
+            ModelReplacementAPI.RegisterSuitModelReplacement("Pajama suit", typeof(BodyReplacementGreywolf));
+
+            //
+
+            Harmony harmony = new Harmony("GreywolfModelReplacement");
+            harmony.PatchAll();
+            Logger.LogInfo($"Plugin {"GreywolfModelReplacement"} is loaded!");
+        }
+
+
+
+        [HarmonyPatch(typeof(PlayerControllerB))]
+        public class PlayerControllerBPatch
+        {
+
+            [HarmonyPatch("Update")]
+            [HarmonyPostfix]
+            public static void UpdatePatch(ref PlayerControllerB __instance)
+            {
+                ModelReplacementAPI.SetPlayerModelReplacement(__instance, typeof(BodyReplacementEruqyuna));
+
+            }
+
+        }
+
+
+
+    }
+
+}
